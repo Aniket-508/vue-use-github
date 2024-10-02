ce72f435fe7c766bb1a17ed999ff0a9cfb151ba9
Aniket-508 <pawaraniket508@gmail.com>
2024-10-02 16:12:00 +0530

    feat: added plugin to generate .ts files in build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({ insertTypesEntry: true, tsconfigPath: "./tsconfig.build.json" }),
+  ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
